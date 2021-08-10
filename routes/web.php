baca7730c8f161db5005f3625dad32a0fd3e47e1
Md Masud <infomasud@gmail.com>
2021-08-10 11:47:54 +0100

    Web template integreted

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\WebController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WebController::class, 'index'])->name('root');
 
 // Route::get('/admin', function(){
 //     return 'admin';
